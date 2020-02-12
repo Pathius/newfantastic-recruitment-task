89944a695feec6c8756e2627af509a7afdecb6cf
Bartosz <bartoszurbanski.biznes@gmail.com>
2020-02-12 10:29:26 +0100

    deleted unecessary files, created footer, imported fonts

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <router-view />
+    <TheFooter />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import Vue from "vue";
+import TheFooter from "./components/TheFooter.vue";
 export default Vue.extend({
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    TheFooter
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Rubik";
 }
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<template functional>
+  <footer class="footer">
+    <p class="footer__text">@2020 Pizza Corp LTD</p>
+  </footer>
+</template>
+<style lang="scss">
+.footer {
+  position: absolute;
+  width: 100%;
+  color: white;
+  bottom: 0;
+  background-color: black;
+  text-align: center;
+  &__text {
+    line-height: 14px;
+    padding: 20px 0;
+    font-size: 12px;
+  }
+}
+</style>
